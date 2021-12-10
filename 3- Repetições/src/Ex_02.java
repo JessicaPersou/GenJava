8cc349081b3d625bc386ad0a2c7b601eb8b58efa
Jessica Sousa <jessica.persou@gmail.com>
2021-12-10 17:42:23 -0300

    Exercícios de Repetição

--- 3- Repetições/src/Ex_02.java
+++ 3- Repetições/src/Ex_02.java
@@ -2,17 +2,21 @@ import java.util.Scanner;
 public class Ex_02 {
 /*Ler 10 números e imprimir quantos são pares e quantos são ímpares. (FOR)*/
 	public static void main(String[] args) {
-		int n, mostraPar=0, mostraImpar=0;
+		int n, x, mostraPar=0, mostraImpar=0;
 		Scanner leia = new Scanner(System.in);
 		
-		for(n=0;n<=10;n++) {
+		for(x=0;x<=10;x++) {
 			System.out.println("Informe o número desejado: ");
 			n = leia.nextInt();
-
-		}if(n%2==0) {
-				System.out.println("Pares são:  " + mostraPar);
-				}else{
-					System.out.println("Impares são: " + mostraImpar);
-			}
+			
+			if(n%2==0) {
+				mostraPar+=1;
+				}
+			if(n%2!=0){
+				mostraImpar+=1;
+			}	
+					
+		}	System.out.println("Pares são:  " + mostraPar);
+			System.out.println("Impares são: " + mostraImpar);	
 	}
 }
