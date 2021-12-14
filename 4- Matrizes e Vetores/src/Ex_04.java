3d19472d00161710908d797c2b6fd54159e6b375
Jessica Sousa <jessica.persou@gmail.com>
2021-12-13 21:32:30 -0300

    Atualização de conteúdo

--- 4- Matrizes e Vetores/src/Ex_04.java
+++ 4- Matrizes e Vetores/src/Ex_04.java
@@ -9,11 +9,11 @@ public class Ex_04 {
 		int matriz[][] = new int[3][3], l,c,soma = 0;
 		for( l=0;l<3;l++) {
 			for( c=0;c<3;c++) {
-				System.out.println("Informe um número: ");
+				System.out.print("Informe um número: ");
 				matriz[l][c]=leia.nextInt();
 			}
-		}
-			System.out.println();
-			System.out.println("Soma diagonal principal:" + (matriz[0][0] + matriz[1][1]+ matriz[2][2]));
+		}	System.out.println();
+			
+			System.out.println("Soma diagonal principal:" + (matriz[0][0] + matriz[1][1]+ matriz[2][2]) + "\t");
 	}
 }
